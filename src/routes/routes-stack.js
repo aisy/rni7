5f3974d01db56136408b0c2cdc8d657227f1be30
aisy <aisymuhammad@gmail.com>
2021-02-20 21:04:26 +0700

    rename router

--- src/routes/routes-stack.js
+++ src/routes/routes-stack.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
+// import navigator from library
 import { createStackNavigator } from '@react-navigation/stack';
 
 // import halaman
 import Home from '../pages/Home';
 import List from '../pages/List';
 
-// membuat stack
+// membuat navigator berdasarkan 
 const Stack = createStackNavigator();
 
 const Router = () => {
   return (
     // membuat container
     <NavigationContainer>
+
       {/* membuat stack */}
       <Stack.Navigator>
-        {/* membuat list Screen */}
         <Stack.Screen name={"Home"} component={Home} />
         <Stack.Screen name={"List"} component={List} />
       </Stack.Navigator>
+
     </NavigationContainer>
   )
 }
