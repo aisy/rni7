db2c2ab7b413f0a6699a7a6402b80c0ebe5010e1
aisy <aisymuhammad@gmail.com>
2021-02-20 21:05:14 +0700

    add many route choices

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import RouterApp from './src/routes/routes';
+
+// Untuk memilih router silahkan di uncomment salah satunya dibawah ini
+
+import RouterStack from './src/routes/routes-stack';
+// import RouterDrawer from './src/routes/routes-drawer';
+// import RouterTabBottom from './src/routes/router-bottom-tab';
+// import RouterTabTop from './src/routes/router-top-tab';
 
 const App = () => {
   return (
-    <RouterApp />
+    <RouterStack />
+    // <RouterDrawer />
+    // <RouterTabBottom />
+    // <RouterTabTop />
   );
 }
 
